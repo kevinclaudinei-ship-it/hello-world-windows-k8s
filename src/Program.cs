b9dd9f7eb650f297f770b0bf04ed1904ec67018a
kevinclaudinei-ship-it <kevinclaudinei@gmail.com>
2026-02-09 10:40:35 -0300

    123

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "teste k8s windows");
+app.MapGet("/", () => "teste k8s windows Pod: ' + $env:COMPUTERNAME + '");
 
 app.Run();
